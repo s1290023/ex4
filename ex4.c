a91e85cdf8255ea73fb9f9492e425e623cd90a58
Akamatsu Daichi <s1290023@u-aizu.ac.jp>
2022-07-13 16:16:53 +0900

    HoT:initial revision

---
+++ ex4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+   printf("Tossing a coin...\n");
+
+  int i,j;
+  int k=0;
+  int l = 0;
+
+  srand(time(NULL));
+  
+  for(i=0;i<3;i++){
+    printf("Round %d:",i+1);
+    
+     j = rand();
+
+     if(j%2 == 0){
+      printf("Heads\n");
+      k++;
+    }
+
+    else if(j%2 != 0){
+      printf("Tails\n");
+      l++;
+    }
+
+  }
+
+  printf("Heads: %d, Tails: %d\n",k,l);
+}
